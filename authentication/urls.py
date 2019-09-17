bdc79ede4471523b108ec2be9840378ae1ab3d6d
AKRAM MUKASA <mukasaakram55@gmail.com>
2019-09-17 10:05:10 +0300

    Update requirements.txt

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,10 +1,11 @@
 
 from django.urls import path
-from authentication.views import RegistrationView
+from authentication.views import (RegistrationView, EmailVerificationView)
 
 
 app_name = 'authentication'
 
 urlpatterns = [
   path("register/", RegistrationView.as_view(), name="register"),
+  path("verify/", EmailVerificationView.as_view(), name="verify")
 ]
--- authentication/views.py
+++ authentication/views.py
@@ -5,6 +5,10 @@ from rest_framework import (
     generics,
     status,
 )
+from rest_framework.settings import settings
+import jwt
+import os
+from authentication.models import User
 from rest_framework.permissions import AllowAny
 from authentication.serializer import RegistrationSerializer
 from authentication.renderers import UserJSONRenderer
@@ -48,6 +52,46 @@ class RegistrationView(APIView):
             }
         }
         return Response(response, status= status.HTTP_201_CREATED)
+
+class EmailVerificationView(APIView):
+
+    def get(self, request):
+        token, user_id = request.GET.get("token").split("~")
+        try:
+            payload = jwt.decode(token, settings.SECRET_KEY)
+        except jwt.exceptions.DecodeError:
+            return self.sendResponse("Verification link is Invalid")
+        except jwt.ExpiredSignatureError:
+            url = generate_validation_url([request], user_id=user_id)
+            user = User.objects.filter(id=user_id).first()
+            payload = {
+                "subject": "A4A events , Verify your Account",
+                "recipient": [user.email],
+                "text_body": "activate_account.txt",
+                "html_body": "activate_account.html",
+                "context": {
+                    'username': user.first_name,
+                    'url': url
+                }
+            }
+            send_email_notification.delay(payload)
+
+            message = "verification link is expired, we have " \
+                      "sent you a new one."
+            return self.sendResponse(message, status.HTTP_400_BAD_REQUEST)
+
+        user = User.objects.filter(email=payload.get("email")).first()
+        if user.is_verified:
+            return self.sendResponse("Account is already activated")
+
+        user.is_verified = True
+        user.save()
+        return  self.sendResponse("Email has been verified", status.HTTP_200_OK)
+
+    def sendResponse(self, message, status=status.HTTP_200_OK):
+        return Response({
+            "message": message
+        }, status)
 
        
         
